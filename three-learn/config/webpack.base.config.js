6f0544cb71c6c5207ae115fcae02fa10b98a525b
zhengjianfeng <283313959@qq.com>
2024-10-12 14:59:06 +0800

    update

--- three-learn/config/webpack.base.config.js
+++ three-learn/config/webpack.base.config.js
@@ -21,37 +21,20 @@ module.exports = {
 			},
 			{
 				test: /\.(png|PNG|jpg|JPG|bmp|BMP|GIF|gif)$/,
-				type: "asset", // 对标到 url-loader
-				parser: {
-					dataUrlCondition: {
-						maxSize: 1024
-					}
-				},
 				use: [
 					{
-						loader: "image-webpack-loader",
+						loader: "url-loader",
 						options: {
-							// mozjpeg：用于压缩 JPG(JPEG) 图片；
-							mozjpeg: {
-								quality: 80
-							},
-							// optipng：用于压缩 PNG 图片；
-							// pngquant：同样用于压缩 PNG 图片；
-							// svgo：用于压缩 SVG 图片；
-							// gifsicle：用于压缩 Gif 图；
-							gifsicle: {
-								optimizationLevel: 2
-							},
-							// webp：用于将 JPG/PNG 图压缩并转化为 WebP 图片格式。
+							limit: 13000
 						}
 					}
 				]
 			}, 
-			{
-				test: /\.(svg|SVG)$/,
-				type: "asset/resource" // 对标到 file-loader
-				// type: "asset/source" // 对标到 raw-loader
-			}
+			// {
+			// 	test: /\.(svg|SVG)$/,
+			// 	type: "asset/resource" // 对标到 file-loader
+			// 	// type: "asset/source" // 对标到 raw-loader
+			// }
 		],
 	},
 	resolve: {
